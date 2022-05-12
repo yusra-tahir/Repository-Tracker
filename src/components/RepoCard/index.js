676ec3a617cd944cc6d591055f7466a21a6a5bbc
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-05-12 12:37:40 +0100

    fixed bugs

--- src/components/RepoCard/index.js
+++ src/components/RepoCard/index.js
@@ -21,7 +21,7 @@ const RepoCard = ( { name, forks, url, stargazers, language, visibility, subscri
             <div className="container">                
                 <div className="card">
                 <div className="card-body" id="cardBody">                    
-                    <p id="cardInfo">URL: <br/><a target='_blank' href={url}><span id="cardSpanLink">{url}</span></a></p>
+                    <p id="cardInfo">URL: <br/><a target='_blank' id="cardSpanLink" href={url}>{url}</a></p>
                     <p id="cardInfo">Default Branch: <br/><span id="cardSpan">{branch}</span></p>                    
                     <p id="cardInfo">Language: <br/><span id="cardSpan">{language}</span></p>
                     <p id="cardInfo">Forks: <br/><span id="cardSpan">{forks}</span></p>
